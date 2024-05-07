3cbc0b1a79a4771f616afdf9fa6a6cb3d0a6c44d
Bert Bos <bert@w3.org>
2024-05-07 19:35:57 +0200

    Do not %-escape most characters in URLs

--- tests/test-328.sh
+++ tests/test-328.sh
@@ -1,5 +1,6 @@
 :
 # https://github.github.com/gfm/#example-328
+# Modified: Do not %-escape the URL
 
 trap 'rm -f $IN $EXPECT $OUT' 0
 IN=`mktemp /tmp/test-XXXXXX`
@@ -11,7 +12,7 @@ cat >$IN <<EOF
 EOF
 
 cat >$EXPECT <<EOF
-<p><a href="/f%C3%B6%C3%B6" title="föö">foo</a></p>
+<p><a href="/föö" title="föö">foo</a></p>
 EOF
 
 gawk '@include "markdown.awk"; { lines = lines $0 "\n" } END { printf "%s", markdown::to_html(lines) }' $IN >$OUT
